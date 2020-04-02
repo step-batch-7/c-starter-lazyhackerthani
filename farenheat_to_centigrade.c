983b0c649ee6a64691719f6382f3eeb85f2f5dd2
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-04-02 19:19:09 +0530

    editted

--- farenheat_to_centigrade.c
+++ farenheat_to_centigrade.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-float get_temparature_in_farenheat(int);
+float get_temparature_in_centigrade(int);
 int main()
 {
-  int principal,year;
-  int temperature_in_celcius;
-  printf("enter temperature in celcius:");
-  scanf("%d", &temperature_in_celcius);
-  float temperature_in_farenheat = get_temparature_in_farenheat(temperature_in_celcius);
-  printf("temperature in farenheat is %d", temperature_in_farenheat);
+  int temperature_in_farenheat;
+  printf("enter temperature in farenheat:");
+  scanf("%d", &temperature_in_farenheat);
+  float temperature_in_centigrade = get_temparature_in_centigrade(temperature_in_farenheat);
+  printf("temperature in centigrade is %f", temperature_in_centigrade);
   return 0;
 }
 
-float get_temparature_in_farenheat(int temperature_in_celcius){
-  return (temperature_in_celcius - 32) * 5 / 9;
+float get_temparature_in_centigrade(int temperature_in_farenheat){
+  return (temperature_in_farenheat - 32) * 5 / 9;
 }
