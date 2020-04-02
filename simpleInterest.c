63ab04171ebe0812ba41f86d55d8f4bebb3b1978
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-04-02 12:24:00 +0530

    simple interest

---
+++ simpleInterest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int getSimpleInterest(int,float,int);
+int main()
+{
+  int principal,year;
+  float interest;
+  printf("enter initial principal balance:");
+  scanf("%d", &principal);
+   printf("enter annual interest rate:");
+  scanf("%f", &interest);
+   printf("enter time (in years):");
+  scanf("%d", &year);
+  float simpleInterest = getSimpleInterest(principal,interest,year);
+  printf("simpleInterest is %f", simpleInterest);
+  return 0;
+}
+
+int getSimpleInterest(int principal,float interest,int year){
+  return principal * (interest/100) * year;
+}
